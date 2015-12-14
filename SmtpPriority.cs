4f40a7baac685fb1041140c03da0299cb23cbd98
Luca <leen15@msn.com>
2015-12-14 12:17:16 +0100

    Create SmtpPriority.cs

---
+++ SmtpPriority.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App27.Mail
+{
+    public enum SmtpPriority : byte
+    {
+        High = 1,
+        Normal = 3,
+        Row = 5
+    }
+}
